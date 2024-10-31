c156b5418c0a70331b594f713dec34e5f9aa501c
Shravan Kumar <kshravan694@gmail.com>
2024-10-31 14:31:48 -0700

    csv of stock tokens, added fn to download all nse stock data for the last 10 years

--- Portfolio.py
+++ Portfolio.py
@@ -1,10 +1,47 @@
-class Portfolio():
-    dateStarted = 0
-    listOfStocks = []
-    dailyReturns = 0
-
-    def __init__(self, listofStocks, dateStarted):
-        self.listOfStocks = listofStocks
-        self.dateStarted = dateStarted
+import yfinance
+import csv
+import random
+import pandas
 
 
+        
+
+# runs n trials with sample size s from the last y years of data
+def runTrials(n:int, s:int, y:int):
+    experiment = Experiment()
+    experiment.buildIndex()
+    for i in range(n):
+        experiment.runTrial
+        
+
+
+
+class Experiment():
+    def __init__(self) -> None:
+        self.index = []
+
+    def buildIndex(self):
+        with open("EQUITY_L.csv", mode= 'r') as file:
+            csvFile = csv.reader(file)
+            for line in csvFile:
+                self.index.append(line[9])
+        return self.index
+    
+    def downloadNSE(self):
+        for stockName in self.buildIndex():
+            str = "data/" + stockName + ".csv"
+            yfinance.download(stockName, period="10y").to_csv(str)
+
+    
+    #runs trial with sample size s and last y years of data
+    def runTrial(self, s:int, y:int):
+        myIndex = self.index.copy()
+        sampleNames = []
+        sampleData = []
+        for i in range(s):
+            random.randint(0, len(self.index))
+            sampleNames.append(myIndex.pop(i))
+   
+
+experiment = Experiment()
+experiment.downloadNSE()
